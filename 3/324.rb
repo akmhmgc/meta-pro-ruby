76535e5eade61dabb2a94c08746f4d54c0cf86f2
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-22 09:53:43 +0900

    p56 メソッドを動的に生成した

---
+++ 3/324.rb
@@ -0,0 +1,19 @@
+class MyClass
+  def self.my_class_method(*args)
+    args.each do |arg|
+      define_method(arg) do 
+        "このメソッドは#{arg}です"
+      end
+    end
+  end
+end
+
+class MyClass2 < MyClass
+  # selfがクラス自身になるので、クラス定義内で呼び出すことができる
+  my_class_method :hoge, :fuga
+end
+
+
+obj = MyClass2.new
+
+p obj.hoge
