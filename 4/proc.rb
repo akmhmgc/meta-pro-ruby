e417781b35d1168a7b39c84f32f1d78bab33ff20
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-26 09:13:18 +0900

    Procオブジェクト

--- 4/proc.rb
+++ 4/proc.rb
@@ -2,4 +2,17 @@ pr = proc do |x|
   p x * 3 
 end
 
-pr.call(3)
+# pr.call(3)
+
+# ブロックを受け取ってProcオブジェクトとして扱う
+def my_method(&block)
+  p block
+  p block.class
+  my_method2(&block)
+end
+
+def my_method2(&block)
+  block.call
+end
+
+my_method{ p 'hello' }
