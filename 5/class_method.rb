94acb21ad614a2c243f1238dd7ede70df6a0e589
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-10 16:26:25 +0900

    クラスメソッドの探索

--- 5/class_method.rb
+++ 5/class_method.rb
@@ -6,8 +6,15 @@ class C
   end
 end
 
+class D < C
+end
+
 # 特異クラスのインスタンスメソッドとして定義
 p C.singleton_class.instance_methods(false) #=> a_class_method
 
 # レシーバの特異メソッド
 p C.methods(false) #=> a_class_method
+
+
+# Dが特異クラスを見る-> Cの特異クラスを見るといった形でメソッドを探索する
+p D.a_class_method
