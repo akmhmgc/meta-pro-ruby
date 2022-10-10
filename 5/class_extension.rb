44c0986bb6fce2be4e1106d7bfaf69cd2ee6d1cc
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-10 17:08:21 +0900

    Extend

--- 5/class_extension.rb
+++ 5/class_extension.rb
@@ -13,3 +13,19 @@ class MyClass
 end
 
 p MyClass.my_method
+
+module MyModule2
+  def my_method2
+    'hello2'
+  end
+end
+
+# 同様のことをextendを用いて達成する
+MyClass.extend MyModule2
+
+p MyClass.my_method2 #=> hello2
+
+# オブジェクトに対してもいける
+obj = MyClass.new
+obj.extend MyModule2
+p obj.my_method2 #=> hello2
